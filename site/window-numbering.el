42130ed1f5f088c86845dd3cf1feee471d383942
Meng Demao <mengdemao19951021@163.com>
2020-11-30 22:15:53 +0800

    fix warning

--- site/window-numbering.el
+++ site/window-numbering.el
@@ -52,7 +52,7 @@
 ;;
 ;;; Code:
 
-(eval-when-compile (require 'cl-lib))
+(eval-when-compile (require 'cl))
 
 (push "^No window numbered .$" debug-ignored-errors)
 
@@ -64,7 +64,7 @@
   "*If non-nil, `window-numbering-mode' assigns 0 to the minibuffer if active."
   :group 'window-numbering
   :type '(choice (const :tag "Off" nil)
-                 (const :tag "On" t)))
+				 (const :tag "On" t)))
 
 (defcustom window-numbering-before-hook nil
   "*Hook called before `window-numbering-mode' starts assigning numbers.
@@ -98,29 +98,29 @@ return a number to have it assigned to the current-window, nil otherwise."
 If prefix ARG is given, delete the window instead of selecting it."
   (interactive "P")
   (let ((windows (car (gethash (selected-frame) window-numbering-table)))
-        window)
-    (if (and (>= i 0) (< i 10)
-             (setq window (aref windows i)))
-        (if arg
-            (delete-window window)
-          (select-window window))
-      (error "No window numbered %s" i))))
+		window)
+	(if (and (>= i 0) (< i 10)
+			 (setq window (aref windows i)))
+		(if arg
+			(delete-window window)
+		  (select-window window))
+	  (error "No window numbered %s" i))))
 
 ;; define interactive functions for keymap
 (dotimes (i 10)
   (eval `(defun ,(intern (format "select-window-%s" i)) (&optional arg)
-           ,(format "Select the window with number %i." i)
-           (interactive "P")
-           (select-window-by-number ,i arg))))
+		   ,(format "Select the window with number %i." i)
+		   (interactive "P")
+		   (select-window-by-number ,i arg))))
 
 (defun window-numbering-calculate-left (windows)
   (let ((i 9) left)
-    (while (>= i 0)
-      (let ((window (aref windows i)))
-        (unless window
-          (push (% (1+ i) 10) left)))
-      (decf i))
-    left))
+	(while (>= i 0)
+	  (let ((window (aref windows i)))
+		(unless window
+		  (push (% (1+ i) 10) left)))
+	  (decf i))
+	left))
 
 (defvar window-numbering-windows nil
   "A vector listing the window for each number.")
@@ -131,69 +131,69 @@ If prefix ARG is given, delete the window instead of selecting it."
 
 (defun window-numbering-assign (window &optional number)
   (if number
-      (if (aref window-numbering-windows number)
-          (progn (message "Number %s assigned to two buffers (%s and %s)"
-                          number window (aref window-numbering-windows number))
-                 nil)
-        (setf (aref window-numbering-windows number) window)
-        (puthash window number window-numbering-numbers)
-        (setq window-numbering-left (delq number window-numbering-left))
-        t)
-    ;; else default adding
-    (when window-numbering-left
-      (unless (gethash window window-numbering-numbers)
-        (let ((number (car window-numbering-left)))
-          (window-numbering-assign window number)
-          number)))))
+	  (if (aref window-numbering-windows number)
+		  (progn (message "Number %s assigned to two buffers (%s and %s)"
+						  number window (aref window-numbering-windows number))
+				 nil)
+		(setf (aref window-numbering-windows number) window)
+		(puthash window number window-numbering-numbers)
+		(setq window-numbering-left (delq number window-numbering-left))
+		t)
+	;; else default adding
+	(when window-numbering-left
+	  (unless (gethash window window-numbering-numbers)
+		(let ((number (car window-numbering-left)))
+		  (window-numbering-assign window number)
+		  number)))))
 
 (defun window-numbering-update ()
   "Update the window numbering for the current frame.
 Optional parameter PREASSIGNED-WINDOWS is a hashmap already mapping some
 windows to numbers."
   (setq window-numbering-windows (make-vector 10 nil)
-        window-numbering-numbers (make-hash-table :size 10)
-        window-numbering-left
-        (window-numbering-calculate-left window-numbering-windows))
+		window-numbering-numbers (make-hash-table :size 10)
+		window-numbering-left
+		(window-numbering-calculate-left window-numbering-windows))
   (puthash (selected-frame)
-           (cons window-numbering-windows window-numbering-numbers)
-           window-numbering-table)
+		   (cons window-numbering-windows window-numbering-numbers)
+		   window-numbering-table)
   (when (and window-numbering-auto-assign-0-to-minibuffer
-             (active-minibuffer-window))
-    (window-numbering-assign (active-minibuffer-window) 0))
+			 (active-minibuffer-window))
+	(window-numbering-assign (active-minibuffer-window) 0))
   (let ((windows (window-list nil 0 (frame-first-window))))
-    (run-hook-with-args 'window-numbering-before-hook windows)
-    (when window-numbering-assign-func
-      (mapc (lambda (window)
-              (with-selected-window window
-                (with-current-buffer (window-buffer window)
-                  (let ((num (funcall window-numbering-assign-func)))
-                    (when num
-                      (window-numbering-assign window num))))))
-            windows))
-    (dolist (window windows)
-      (window-numbering-assign window))))
+	(run-hook-with-args 'window-numbering-before-hook windows)
+	(when window-numbering-assign-func
+	  (mapc (lambda (window)
+			  (with-selected-window window
+				(with-current-buffer (window-buffer window)
+				  (let ((num (funcall window-numbering-assign-func)))
+					(when num
+					  (window-numbering-assign window num))))))
+			windows))
+	(dolist (window windows)
+	  (window-numbering-assign window))))
 
 (defun window-numbering-get-number-string (&optional window)
   (let ((s (int-to-string (window-numbering-get-number window))))
-    (propertize s 'face 'window-numbering-face)))
+	(propertize s 'face 'window-numbering-face)))
 
 (defun window-numbering-get-number (&optional window)
   (gethash (or window (selected-window))
-           (cdr (gethash (selected-frame) window-numbering-table))))
+		   (cdr (gethash (selected-frame) window-numbering-table))))
 
 (defvar window-numbering-keymap
   (let ((map (make-sparse-keymap)))
-    (define-key map "\M-0" 'select-window-0)
-    (define-key map "\M-1" 'select-window-1)
-    (define-key map "\M-2" 'select-window-2)
-    (define-key map "\M-3" 'select-window-3)
-    (define-key map "\M-4" 'select-window-4)
-    (define-key map "\M-5" 'select-window-5)
-    (define-key map "\M-6" 'select-window-6)
-    (define-key map "\M-7" 'select-window-7)
-    (define-key map "\M-8" 'select-window-8)
-    (define-key map "\M-9" 'select-window-9)
-    map)
+	(define-key map "\M-0" 'select-window-0)
+	(define-key map "\M-1" 'select-window-1)
+	(define-key map "\M-2" 'select-window-2)
+	(define-key map "\M-3" 'select-window-3)
+	(define-key map "\M-4" 'select-window-4)
+	(define-key map "\M-5" 'select-window-5)
+	(define-key map "\M-6" 'select-window-6)
+	(define-key map "\M-7" 'select-window-7)
+	(define-key map "\M-8" 'select-window-8)
+	(define-key map "\M-9" 'select-window-9)
+	map)
   "Keymap used in by `window-numbering-mode'.")
 
 ;;;###autoload
@@ -201,47 +201,47 @@ windows to numbers."
   "A minor mode that assigns a number to each window."
   nil nil window-numbering-keymap :global t
   (if window-numbering-mode
-      (unless window-numbering-table
-        (save-excursion
-          (setq window-numbering-table (make-hash-table :size 16))
-          (window-numbering-install-mode-line)
-          (add-hook 'minibuffer-setup-hook 'window-numbering-update)
-          (add-hook 'window-configuration-change-hook
-                    'window-numbering-update)
-          (dolist (frame (frame-list))
-            (select-frame frame)
-            (window-numbering-update))))
-    (window-numbering-clear-mode-line)
-    (remove-hook 'minibuffer-setup-hook 'window-numbering-update)
-    (remove-hook 'window-configuration-change-hook
-                 'window-numbering-update)
-    (setq window-numbering-table nil)))
+	  (unless window-numbering-table
+		(save-excursion
+		  (setq window-numbering-table (make-hash-table :size 16))
+		  (window-numbering-install-mode-line)
+		  (add-hook 'minibuffer-setup-hook 'window-numbering-update)
+		  (add-hook 'window-configuration-change-hook
+					'window-numbering-update)
+		  (dolist (frame (frame-list))
+			(select-frame frame)
+			(window-numbering-update))))
+	(window-numbering-clear-mode-line)
+	(remove-hook 'minibuffer-setup-hook 'window-numbering-update)
+	(remove-hook 'window-configuration-change-hook
+				 'window-numbering-update)
+	(setq window-numbering-table nil)))
 
 (defun window-numbering-install-mode-line (&optional position)
   "Install the window number from `window-numbering-mode' to the mode-line."
   (let ((mode-line (default-value 'mode-line-format))
-        (res))
-    (dotimes (i (min (or position window-numbering-mode-line-position)
-                     (length mode-line)))
-      (push (car mode-line) res)
-      (pop mode-line))
-    (push '(:eval (window-numbering-get-number-string)) res)
-    (while mode-line
-      (push (car mode-line) res)
-      (pop mode-line))
-    (setq-default mode-line-format (nreverse res)))
+		(res))
+	(dotimes (i (min (or position window-numbering-mode-line-position)
+					 (length mode-line)))
+	  (push (car mode-line) res)
+	  (pop mode-line))
+	(push '(:eval (window-numbering-get-number-string)) res)
+	(while mode-line
+	  (push (car mode-line) res)
+	  (pop mode-line))
+	(setq-default mode-line-format (nreverse res)))
   (force-mode-line-update t))
 
 (defun window-numbering-clear-mode-line ()
   "Remove the window number of `window-numbering-mode' from the mode-line."
   (let ((mode-line (default-value 'mode-line-format))
-        (res))
-    (while mode-line
-      (let ((item (car mode-line)))
-        (unless (equal item '(:eval (window-numbering-get-number-string)))
-          (push item res)))
-      (pop mode-line))
-    (setq-default mode-line-format (nreverse res)))
+		(res))
+	(while mode-line
+	  (let ((item (car mode-line)))
+		(unless (equal item '(:eval (window-numbering-get-number-string)))
+		  (push item res)))
+	  (pop mode-line))
+	(setq-default mode-line-format (nreverse res)))
   (force-mode-line-update t))
 
 (provide 'window-numbering)
