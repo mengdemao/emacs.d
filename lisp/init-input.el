33df48ea38d9bba7e066ad493cd2fa629998d046
Meng Demao <mengdemao19951021@163.com>
2021-01-16 12:03:09 +0800

    更新输入法

--- lisp/init-input.el
+++ lisp/init-input.el
@@ -1,7 +1,20 @@
+;;; init-input.el --- Input Method -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
 (use-package rime
   :ensure t
   :custom
-  (default-input-method "rime"))
+  (default-input-method "rime")
+  :config
+  (setq rime-posframe-properties
+		(list :background-color "#333333"
+			  :foreground-color "#dcdccc"
+			  :font "YaHei Consolas Hybrid-14"
+			  :internal-border-width 10))
+
+  (setq default-input-method "rime"
+		rime-show-candidate 'posframe))
 
 (provide 'init-input)
-;;; init_input.el ends here
+;;; init-input.el ends here
